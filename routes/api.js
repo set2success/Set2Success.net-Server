1681c51b322df2626fd37c0f03e8f5a18c0be271
Pankajkumar90 <pankajbaliyan90@gmail.com>
2023-12-25 00:31:51 +0530

    Fix formatting and syntax errors in code

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
 const api = require('express')();
 const lmsRouter = require('./lms.routes');
 const authRouter = require('./auth');
-const espRouter = require('./esp.routes')
+const espRouter = require('./esp.routes');
 
 api.use('/auth', authRouter);
 api.use('/lms', lmsRouter);
-api.use('/esp',espRouter)
+api.use('/esp', espRouter);
 
 module.exports = api;
