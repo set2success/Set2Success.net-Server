c7c6bdc0408d36cef3353aa567cd778614465929
Pankajkumar90 <pankajbaliyan90@gmail.com>
2024-01-13 23:09:37 +0530

    Add cart controller and cart model

---
+++ controller/cart.controller.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const CartModel = require('../models/CartModal');
+const AddToCart = async (req, res) => {
+    try {
+        const { userId, itemName, price, quantity } = req.body;
+        const myCart = await CartModel.findOne({ user: userId });
+
+        if (!myCart) {
+            const newCart = new CartModel({
+                user: userId,
+                cartItems: [],
+            });
+            newCart.cartItems.push({
+                item: itemName,
+                price: price,
+                quantity: quantity,
+            });
+            await newCart.save();
+            res.status(200).json(newCart);
+        }
+
+        if (myCart) {
+            const itemIndex = myCart.cartItems.findIndex(
+                (p) => p.item === itemName,
+            );
+            if (itemIndex > -1) {
+                let cartItem = myCart.cartItems[itemIndex];
+                cartItem.price = price;
+                cartItem.quantity = quantity;
+                myCart.cartItems[itemIndex] = cartItem;
+            } else {
+                myCart.cartItems.push({
+                    item: itemName,
+                    price: price,
+                    quantity: quantity,
+                });
+            }
+            await myCart.save();
+            res.status(200).json(myCart);
+        }
+    } catch (error) {
+        console.error('Error adding to cart:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+const ReadCart = (req, res) => {
+    const { userId } = req.body;
+    console.log('userId', userId);
+
+    CartModel.findOne({ userId: userId })
+        .then((cart) => {
+            res.status(200).json(cart);
+        })
+        .catch((err) => {
+            res.status(500).json({
+                message: 'Error finding cart',
+                error: err.message,
+            });
+        });
+};
+
+// export functions
+module.exports = {
+    AddToCart,
+    ReadCart,
+};
---
+++ models/CartModal.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        cartItems: [
+            {
+                item: { type: String, required: true },
+                quantity: { type: Number, default: 1 },
+                price: { type: Number, required: true },
+            },
+        ],
+    },
+    { timestamps: true },
+);
+
+const CartModel = mongoose.model('cart', cartSchema);
+
+module.exports = CartModel;
